92d4b72806d19ca80e3e0330c6953d6bd7b8f262
Wendel Scardua <wendelscardua@gmail.com>
2023-09-30 13:42:49 -0300

    Add polyominos header

---
+++ src/polyominos.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <array>
+
+#include "common.hpp"
+
+#define NUM_POLYOMINOS 93
+
+struct Coordinates {
+  s8 delta_row;
+  s8 delta_column;
+};
+
+struct Kick {
+  std::array<Coordinates, 5> deltas;
+};
+
+struct PolyominoDef {
+  PolyominoDef* left_rotation;
+  PolyominoDef* right_rotation;
+  Kick* left_kick;
+  Kick* right_kick;
+  u8 size;
+  std::array<Coordinates, 5> deltas;
+};
+
+extern "C" const std::array<PolyominoDef*, NUM_POLYOMINOS> polyominos;
