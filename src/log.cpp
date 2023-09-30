ba2c80cefaf11e6fd99c5e7ce12d1b92aede0611
Wendel Scardua <wendelscardua@gmail.com>
2023-09-30 10:40:06 -0300

    Helpers for logging in hex

--- src/log.cpp
+++ src/log.cpp
@@ -1,3 +1,16 @@
 #include "log.hpp"
+#include "common.hpp"
 
 void __putchar(char c) { POKE(0x4018, c); }
+void put_hexdigit(u8 h) {
+  if (h < 10) __putchar('0' + h);
+  else __putchar('a' + h - 10);
+}
+void put_hex(u8 h) {
+  put_hexdigit(h >> 4);
+  put_hexdigit(h & 0x0f);
+}
+void put_hex(u16 h) {
+  put_hex((u8)(h >> 8));
+  put_hex((u8)h);
+}
--- src/log.hpp
+++ src/log.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "common.hpp"
 #include <peekpoke.h>
 
 extern "C" void __putchar(char c);
+void put_hexdigit(u8 h);
+void put_hex(u8 h);
+void put_hex(u16 h);
