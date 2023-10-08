b5c4eb7a18594efc5f5feefa090f850251d897e4
Wendel Scardua <wendelscardua@gmail.com>
2023-10-08 01:45:22 -0300

    Randomize bag

--- src/bag.hpp
+++ src/bag.hpp
@@ -3,6 +3,7 @@
 #include "common.hpp"
 #include <array>
 #include <cstddef>
+#include <neslib.h>
 
 template <typename T, size_t N> class Bag {
   std::array<T, N> items;
@@ -11,45 +12,51 @@ template <typename T, size_t N> class Bag {
   void (*refill)(Bag *bag);
 
 public:
-  Bag(void (*refill)(Bag *bag));
+  Bag(void (*refill)(Bag *bag)) : index(0), end(0), refill(refill){};
 
-  void insert(T value);
+  void insert(T value) {
+    u8 random_point = index + (((u16)rand8() * (u16)(size() + 1)) >> 8);
+    if (random_point >= N) {
+      random_point -= N;
+    }
 
-  T take();
+    if (random_point == end) {
+      items[end++] = value;
+    } else {
+      items[end++] = items[random_point];
+      items[random_point] = value;
+    }
+    if (end == N) {
+      end = 0;
+    }
+  }
 
-  T peek();
+  T take() {
+    if (empty()) {
+      refill(this);
+    }
+    u8 old_index = index;
+    index++;
+    if (index == N) {
+      index = 0;
+    }
+    return items[old_index];
+  }
 
-  bool empty();
+  T peek() {
+    if (empty()) {
+      refill(this);
+    }
+    return items[index];
+  }
+
+  bool empty() { return index == end; }
+
+  size_t size() {
+    if (end >= index) {
+      return end - index;
+    } else {
+      return end + N - index;
+    }
+  }
 };
-
-template <typename T, size_t N>
-Bag<T, N>::Bag(void (*refill)(Bag *bag)) : index(0), end(0), refill(refill) {};
-
-template <typename T, size_t N> void Bag<T, N>::insert(T value) {
-  items[end] = value;
-  end++;
-  if (end == N) {
-    end = 0;
-  }
-}
-
-template <typename T, size_t N> T Bag<T, N>::take() {
-  if (empty()) {
-    refill(this);
-  }
-  u8 old_index = index;
-  index++;
-  if (index == N) {
-    index = 0;
-  }
-  return items[old_index];
-}
-
-template <typename T, size_t N> T Bag<T, N>::peek() {
-  if (empty()) {
-    refill(this);
-  }
-  return items[index];
-}
-
-template <typename T, size_t N> bool Bag<T, N>::empty() { return index == end; }
