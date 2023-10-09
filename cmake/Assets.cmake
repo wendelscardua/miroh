62f565edadfe82c61adcd3788fb033041affade1
Wendel Scardua <wendelscardua@gmail.com>
2023-10-09 19:00:16 -0300

    Allow different bg chr for Web and NES releases

--- cmake/Assets.cmake
+++ cmake/Assets.cmake
@@ -1,6 +1,6 @@
 function(add_raw_asset)
   set(options)
-  set(oneValueArgs SOURCE)
+  set(oneValueArgs SOURCE TARGET)
   set(multiValueArgs)
   cmake_parse_arguments(ASSET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
@@ -9,7 +9,12 @@ function(add_raw_asset)
   endif()
 
   set(ASSET_SRC "${CMAKE_SOURCE_DIR}/assets/${ASSET_SOURCE}")
-  set(ASSET_DEST "${CMAKE_BINARY_DIR}/assets/${ASSET_SOURCE}")
+
+  if (ASSET_TARGET)
+    set(ASSET_DEST "${CMAKE_BINARY_DIR}/assets/${ASSET_TARGET}")
+  else()
+    set(ASSET_DEST "${CMAKE_BINARY_DIR}/assets/${ASSET_SOURCE}")
+  endif()
 
   get_filename_component(BASE_NAME ${ASSET_SRC} NAME)
 
@@ -24,7 +29,7 @@ endfunction()
 
 function(add_donut_asset)
   set(options)
-  set(oneValueArgs SOURCE)
+  set(oneValueArgs SOURCE TARGET)
   set(multiValueArgs)
   cmake_parse_arguments(ASSET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
@@ -41,16 +46,20 @@ function(add_donut_asset)
     message(FATAL_ERROR "The donut tool is required!")
   endif()
 
-  get_filename_component(BASE_NAME ${ASSET_SOURCE} NAME)
-
   set(ASSET_SRC "${CMAKE_SOURCE_DIR}/assets/${ASSET_SOURCE}")
-  set(ASSET_DEST "${CMAKE_BINARY_DIR}/assets/${ASSET_SOURCE}.donut")
+
+  if (ASSET_TARGET)
+    set(ASSET_DEST "${CMAKE_BINARY_DIR}/assets/${ASSET_TARGET}")
+  else()
+    set(ASSET_DEST "${CMAKE_BINARY_DIR}/assets/${ASSET_SOURCE}.donut")
+  endif()
+
+  get_filename_component(BASE_NAME ${ASSET_DEST} NAME)
 
   add_custom_command(
     OUTPUT ${ASSET_DEST}
     COMMAND ${DONUT_TOOL} -f ${ASSET_SRC} -o ${ASSET_DEST}
     DEPENDS ${ASSET_SRC}
   )
-
-  add_custom_target("${BASE_NAME}.donut" DEPENDS ${ASSET_DEST})
+  add_custom_target(${BASE_NAME} DEPENDS ${ASSET_DEST})
 endfunction()
