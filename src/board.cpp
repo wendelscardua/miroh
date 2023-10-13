1cd52ce5d6d9b14f2459aa534a7fb12b54c4ddd3
Wendel Scardua <wendelscardua@gmail.com>
2023-10-13 18:39:56 -0300

    Formatting

--- src/board.cpp
+++ src/board.cpp
@@ -25,7 +25,7 @@ void Cell::reset() {
 
 Cell *Cell::representative() {
   Cell *temp = this;
-  while(temp != temp->parent) {
+  while (temp != temp->parent) {
     temp->parent = temp->parent->parent;
     temp = temp->parent;
   }
@@ -35,12 +35,14 @@ Cell *Cell::representative() {
 void Cell::join(Cell *other) {
   Cell *x = this->representative();
   Cell *y = other->representative();
-  if (x != y) y->parent = x;
+  if (x != y)
+    y->parent = x;
 }
 
-Board::Board(u8 origin_x, u8 origin_y) : origin_x(origin_x), origin_y(origin_y) {
+Board::Board(u8 origin_x, u8 origin_y)
+    : origin_x(origin_x), origin_y(origin_y) {
   // reset tally
-  for(u8 i = 0; i < HEIGHT; i++) {
+  for (u8 i = 0; i < HEIGHT; i++) {
     tally[i] = 0;
     deleted[i] = 0;
   }
@@ -52,19 +54,20 @@ Board::Board(u8 origin_x, u8 origin_y) : origin_x(origin_x), origin_y(origin_y) 
   dropping_row = -1;
 
   // reset walls
-  for(u8 i = 0; i < HEIGHT; i++) {
-    for(u8 j = 0; j < WIDTH; j++) {
+  for (u8 i = 0; i < HEIGHT; i++) {
+    for (u8 j = 0; j < WIDTH; j++) {
       cell[i][j].reset();
     }
   }
 
-#define NEED_WALL(direction) (template_cell.maybe_##direction##_wall && ((rand8() & 0b11) == 0))
+#define NEED_WALL(direction)                                                   \
+  (template_cell.maybe_##direction##_wall && ((rand8() & 0b11) == 0))
   banked_lambda(GET_BANK(mazes), [this]() {
     static_assert(sizeof(TemplateCell) == 1, "TemplateCell is too big");
 
     // read required walls from template
-    for(u8 i = 0; i < HEIGHT; i++) {
-      for(u8 j = 0; j < WIDTH; j++) {
+    for (u8 i = 0; i < HEIGHT; i++) {
+      for (u8 j = 0; j < WIDTH; j++) {
         TemplateCell template_cell = mazes[maze]->template_cells[i][j];
         if (template_cell.value != 0xff) {
           cell[i][j].walls = template_cell.walls;
@@ -73,14 +76,14 @@ Board::Board(u8 origin_x, u8 origin_y) : origin_x(origin_x), origin_y(origin_y) 
     }
 
     // read "maybe" walls from template
-    for(u8 i = 0; i < HEIGHT; i++) {
-      for(u8 j = 0; j < WIDTH; j++) {
+    for (u8 i = 0; i < HEIGHT; i++) {
+      for (u8 j = 0; j < WIDTH; j++) {
         TemplateCell template_cell = mazes[maze]->template_cells[i][j];
 
         if (template_cell.value == 0xff) {
           // use the old berzerk algorithm
           // assumes the cell is on the valid range
-          switch(rand8() & 0b11) {
+          switch (rand8() & 0b11) {
           case 0:
             cell[i][j].right_wall = true;
             cell[i][j + 1].left_wall = true;
@@ -103,80 +106,95 @@ Board::Board(u8 origin_x, u8 origin_y) : origin_x(origin_x), origin_y(origin_y) 
 
         if (NEED_WALL(up)) {
           cell[i][j].up_wall = true;
-          if (i > 0) { cell[i - 1][j].down_wall = true; }
+          if (i > 0) {
+            cell[i - 1][j].down_wall = true;
+          }
         }
         if (NEED_WALL(down)) {
           cell[i][j].down_wall = true;
-          if (i < HEIGHT - 1) { cell[i + 1][j].up_wall = true; }
+          if (i < HEIGHT - 1) {
+            cell[i + 1][j].up_wall = true;
+          }
         }
         if (NEED_WALL(left)) {
           cell[i][j].left_wall = true;
-          if (j > 0) { cell[i][j - 1].right_wall = true; }
+          if (j > 0) {
+            cell[i][j - 1].right_wall = true;
+          }
         }
         if (NEED_WALL(right)) {
           cell[i][j].right_wall = true;
-          if (j < WIDTH - 1) { cell[i][j + 1].left_wall = true; }
+          if (j < WIDTH - 1) {
+            cell[i][j + 1].left_wall = true;
+          }
         }
       }
     }
   });
 
   // border walls
-  for(u8 i = 0; i < HEIGHT; i++) {
+  for (u8 i = 0; i < HEIGHT; i++) {
     cell[i][0].left_wall = true;
     cell[i][WIDTH - 1].right_wall = true;
   }
 
-  for(u8 j = 0; j < WIDTH; j++) {
+  for (u8 j = 0; j < WIDTH; j++) {
     cell[0][j].up_wall = true;
     cell[HEIGHT - 1][j].down_wall = true;
   }
 
   // union-find-ish-ly ensure all cells are reachable
-  for(u8 i = 0; i < HEIGHT; i++) {
-    for(u8 j = 0; j < WIDTH; j++) {
+  for (u8 i = 0; i < HEIGHT; i++) {
+    for (u8 j = 0; j < WIDTH; j++) {
       auto current_cell = &cell[i][j];
       if (j < WIDTH - 1 && !current_cell->right_wall) {
-        current_cell->join(&cell[i][j+1]);
+        current_cell->join(&cell[i][j + 1]);
       }
       if (i < HEIGHT - 1 && !current_cell->down_wall) {
-        current_cell->join(&cell[i+1][j]);
+        current_cell->join(&cell[i + 1][j]);
       }
     }
   }
 
-  Bag<u8, HEIGHT> row_bag([](auto * bag){
-    for(u8 i = 0; i < HEIGHT; i++) { bag->insert(i); }
+  Bag<u8, HEIGHT> row_bag([](auto *bag) {
+    for (u8 i = 0; i < HEIGHT; i++) {
+      bag->insert(i);
+    }
   });
-  Bag<u8, WIDTH> column_bag([](auto * bag){
-    for(u8 j = 0; j < WIDTH; j++) { bag->insert(j); }
+  Bag<u8, WIDTH> column_bag([](auto *bag) {
+    for (u8 j = 0; j < WIDTH; j++) {
+      bag->insert(j);
+    }
   });
 
   // randomize order of rows, then within a row, the order of columns
-  for(u8 rows = 0; rows < HEIGHT; rows++) {
+  for (u8 rows = 0; rows < HEIGHT; rows++) {
     u8 i = row_bag.take();
-    for(u8 columns = 0; columns < WIDTH; columns++) {
+    for (u8 columns = 0; columns < WIDTH; columns++) {
       u8 j = column_bag.take();
       auto current_cell = &cell[i][j];
       auto right_cell = j < WIDTH - 1 ? &cell[i][j + 1] : NULL;
-      auto down_cell = i < HEIGHT -1 ? &cell[i + 1][j] : NULL;
+      auto down_cell = i < HEIGHT - 1 ? &cell[i + 1][j] : NULL;
 
       // randomize if we are looking first horizontally or vertically
       bool down_first = rand8() & 0b1;
 
-      if (down_first && down_cell && current_cell->representative() != down_cell->representative()) {
+      if (down_first && down_cell &&
+          current_cell->representative() != down_cell->representative()) {
         current_cell->down_wall = false;
         down_cell->up_wall = false;
         down_cell->join(current_cell);
       }
 
-      if (right_cell && current_cell->representative() != right_cell->representative()) {
+      if (right_cell &&
+          current_cell->representative() != right_cell->representative()) {
         current_cell->right_wall = false;
         right_cell->left_wall = false;
         right_cell->join(current_cell);
       }
 
-      if (!down_first && down_cell && current_cell->representative() != down_cell->representative()) {
+      if (!down_first && down_cell &&
+          current_cell->representative() != down_cell->representative()) {
         current_cell->down_wall = false;
         down_cell->up_wall = false;
         down_cell->join(current_cell);
@@ -191,8 +209,8 @@ Board::Board(u8 origin_x, u8 origin_y) : origin_x(origin_x), origin_y(origin_y) 
   //  + + + We want to mark on each of them that they don't
   //  |   | need that "+" in the middle
   //  +-+-+
-  for(u8 row = 0; row < HEIGHT - 1; row++) {
-    for(u8 column = 0; column < WIDTH - 1; column++) {
+  for (u8 row = 0; row < HEIGHT - 1; row++) {
+    for (u8 column = 0; column < WIDTH - 1; column++) {
       auto current_cell = &cell[row][column];
       if (!current_cell->right_wall && !current_cell->down_wall) {
         auto right_down_cell = &cell[row + 1][column + 1];
@@ -213,14 +231,16 @@ Board::Board(u8 origin_x, u8 origin_y) : origin_x(origin_x), origin_y(origin_y) 
 Board::~Board() {}
 
 void Board::render() {
-  for(s8 i = 0; i < HEIGHT; i++) {
-    for(s8 j = 0; j < WIDTH; j++) {
+  for (s8 i = 0; i < HEIGHT; i++) {
+    for (s8 j = 0; j < WIDTH; j++) {
       restore_maze_cell(i, j);
       flush_vram_update2();
     }
   }
-  for(u8 meta_x = origin_x >> 4; meta_x < ((origin_x >> 4) + WIDTH); meta_x++) {
-    for(u8 meta_y = origin_y >> 4; meta_y < ((origin_y >> 4) + HEIGHT); meta_y++) {
+  for (u8 meta_x = origin_x >> 4; meta_x < ((origin_x >> 4) + WIDTH);
+       meta_x++) {
+    for (u8 meta_y = origin_y >> 4; meta_y < ((origin_y >> 4) + HEIGHT);
+         meta_y++) {
       Attributes::set(meta_x, meta_y, WALL_ATTRIBUTE);
     }
   }
@@ -228,12 +248,11 @@ void Board::render() {
 }
 
 bool Board::occupied(s8 row, s8 column) {
-  if (column < 0 ||
-      column > WIDTH - 1 ||
-      row > HEIGHT - 1)
+  if (column < 0 || column > WIDTH - 1 || row > HEIGHT - 1)
     return true;
 
-  if (row < 0) return false;
+  if (row < 0)
+    return false;
 
   return cell[row][column].occupied;
 }
@@ -253,16 +272,19 @@ void Board::free(s8 row, s8 column) {
 }
 
 void Board::block_maze_cell(s8 row, s8 column) {
-  int position = NTADR_A((origin_x >> 3) + (column << 1), (origin_y >> 3) + (row << 1));
+  int position =
+      NTADR_A((origin_x >> 3) + (column << 1), (origin_y >> 3) + (row << 1));
   multi_vram_buffer_horz((const u8[2]){0x74, 0x75}, 2, position);
-  multi_vram_buffer_horz((const u8[2]){0x84, 0x85}, 2, position+0x20);
-  Attributes::set((u8) ((origin_x >> 4) + column), (u8) ((origin_y >> 4) + row), BLOCK_ATTRIBUTE);
+  multi_vram_buffer_horz((const u8[2]){0x84, 0x85}, 2, position + 0x20);
+  Attributes::set((u8)((origin_x >> 4) + column), (u8)((origin_y >> 4) + row),
+                  BLOCK_ATTRIBUTE);
   occupy(row, column);
 }
 
 void Board::restore_maze_cell(s8 row, s8 column) {
   auto current_cell = &cell[row][column];
-  int position = NTADR_A((origin_x >> 3) + (column << 1), (origin_y >> 3) + (row << 1));
+  int position =
+      NTADR_A((origin_x >> 3) + (column << 1), (origin_y >> 3) + (row << 1));
   char metatile_top[2];
   char metatile_bottom[2];
 
@@ -330,7 +352,8 @@ void Board::restore_maze_cell(s8 row, s8 column) {
     if (current_cell->right_wall) { // !down right
       metatile_bottom[1] = TILE_BASE + 2;
     } else { // !down !right
-      if (row == HEIGHT - 1 || column == WIDTH - 1 || current_cell->empty_bottom_right)
+      if (row == HEIGHT - 1 || column == WIDTH - 1 ||
+          current_cell->empty_bottom_right)
         metatile_bottom[1] = TILE_BASE + 0;
       else
         metatile_bottom[1] = TILE_BASE + 11;
@@ -345,9 +368,10 @@ void Board::restore_maze_cell(s8 row, s8 column) {
   }
 
   multi_vram_buffer_horz(metatile_top, 2, position);
-  multi_vram_buffer_horz(metatile_bottom, 2, position+0x20);
+  multi_vram_buffer_horz(metatile_bottom, 2, position + 0x20);
 
-  Attributes::set((u8) ((origin_x >> 4) + column), (u8) ((origin_y >> 4) + row), WALL_ATTRIBUTE);
+  Attributes::set((u8)((origin_x >> 4) + column), (u8)((origin_y >> 4) + row),
+                  WALL_ATTRIBUTE);
 
   free(row, column);
 }
@@ -356,7 +380,7 @@ bool Board::ongoing_line_clearing() {
   bool ongoing = false;
 
   if (cracking_row < 0) {
-    for(s8 i = erasing_row + 1; i < HEIGHT; i++) {
+    for (s8 i = erasing_row + 1; i < HEIGHT; i++) {
       if (tally[i] == WIDTH) {
         deleted[i] = true;
         cracking_row = i;
@@ -367,10 +391,12 @@ bool Board::ongoing_line_clearing() {
     }
   } else {
     ongoing = true;
-    Attributes::set((u8) ((origin_x >> 4) + cracking_column), (u8) ((origin_y >> 4) + cracking_row), FLASH_ATTRIBUTE);
-    int position = NTADR_A((origin_x >> 3) + (cracking_column << 1), (origin_y >> 3) + (cracking_row << 1));
+    Attributes::set((u8)((origin_x >> 4) + cracking_column),
+                    (u8)((origin_y >> 4) + cracking_row), FLASH_ATTRIBUTE);
+    int position = NTADR_A((origin_x >> 3) + (cracking_column << 1),
+                           (origin_y >> 3) + (cracking_row << 1));
     multi_vram_buffer_horz((const u8[2]){0x76, 0x77}, 2, position);
-    multi_vram_buffer_horz((const u8[2]){0x86, 0x87}, 2, position+0x20);
+    multi_vram_buffer_horz((const u8[2]){0x86, 0x87}, 2, position + 0x20);
 
     cracking_column++;
     if (cracking_column == WIDTH) {
@@ -391,8 +417,9 @@ bool Board::ongoing_line_clearing() {
     if (erasing_column == WIDTH) {
       erasing_column = 0;
 
-      for(erasing_row++; erasing_row < HEIGHT; erasing_row++) {
-        if (tally[erasing_row] == WIDTH) break;
+      for (erasing_row++; erasing_row < HEIGHT; erasing_row++) {
+        if (tally[erasing_row] == WIDTH)
+          break;
       }
 
       if (erasing_row == HEIGHT) {
@@ -402,9 +429,10 @@ bool Board::ongoing_line_clearing() {
   }
 
   if (dropping_column < 0) {
-    if (!ongoing && erasing_row < 0 && cracking_row < 0 && line_gravity_enabled) {
+    if (!ongoing && erasing_row < 0 && cracking_row < 0 &&
+        line_gravity_enabled) {
       // dropping column will only start doing stuff if it ever becomes zero
-      for(auto deleted_row : deleted) {
+      for (auto deleted_row : deleted) {
         if (deleted_row) {
           dropping_column = 0;
           dropping_row = -1;
@@ -416,23 +444,25 @@ bool Board::ongoing_line_clearing() {
   } else if (dropping_row < 0) {
     ongoing = true;
 
-    for(s8 i = 0; i < HEIGHT; i++) {
+    for (s8 i = 0; i < HEIGHT; i++) {
       dropping_column_new_states[i] = occupied(i, dropping_column);
     }
 
     s8 current_row = HEIGHT - 1;
     s8 source_row = current_row;
 
-    while(current_row >= 0) {
+    while (current_row >= 0) {
       while (source_row >= 0 && deleted[source_row]) {
         source_row--;
       }
       if (source_row < current_row) {
-        bool source_occupied = source_row < 0 ? false : dropping_column_new_states[source_row];
+        bool source_occupied =
+            source_row < 0 ? false : dropping_column_new_states[source_row];
 
         if (source_occupied) {
           dropping_column_new_states[current_row] = true;
-          if (source_row >= 0) dropping_column_new_states[source_row] = false;
+          if (source_row >= 0)
+            dropping_column_new_states[source_row] = false;
         } else {
           dropping_column_new_states[current_row] = false;
         }
@@ -447,8 +477,10 @@ bool Board::ongoing_line_clearing() {
 
     // we don't have budget to do too many changes per frame
     // so we arbitrarily limit them
-    for(u8 changes = 0; dropping_row >= 0 && changes < LINE_CLEARING_BUDGET; dropping_row--) {
-      if (dropping_column_new_states[dropping_row] != occupied(dropping_row, dropping_column)) {
+    for (u8 changes = 0; dropping_row >= 0 && changes < LINE_CLEARING_BUDGET;
+         dropping_row--) {
+      if (dropping_column_new_states[dropping_row] !=
+          occupied(dropping_row, dropping_column)) {
         changes++;
         if (dropping_column_new_states[dropping_row]) {
           block_maze_cell(dropping_row, dropping_column);
@@ -461,7 +493,7 @@ bool Board::ongoing_line_clearing() {
     if (dropping_row < 0) {
       dropping_column++;
       if (dropping_column == WIDTH) {
-        for(s8 i = 0; i < HEIGHT; i++) {
+        for (s8 i = 0; i < HEIGHT; i++) {
           deleted[i] = false;
         }
         dropping_column = -1;
