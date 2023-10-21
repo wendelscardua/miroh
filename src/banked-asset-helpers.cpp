a6d8cfd731c19e5a7d1fc4fd1378fe24950606fd
Wendel Scardua <wendelscardua@gmail.com>
2023-10-20 21:42:29 -0300

    Scoped bank with banked metasprites

--- src/banked-asset-helpers.cpp
+++ src/banked-asset-helpers.cpp
@@ -1,16 +1,9 @@
 #include "banked-asset-helpers.hpp"
 #include "bank-helper.hpp"
-#include "common.hpp"
 #include <bank.h>
 #include <neslib.h>
 
 void banked_oam_meta_spr(char x, char y, const void *data) {
-  u8 old_bank = get_prg_bank();
-  u8 bank = GET_BANK(metasprite_list);
-
-  set_prg_bank(bank);
-
+  ScopedBank scopedBank(GET_BANK(metasprite_list));
   oam_meta_spr(x, y, data);
-
-  set_prg_bank(old_bank);
 }
