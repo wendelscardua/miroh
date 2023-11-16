619a7169933be565b6f9e9adb99ae6401a53ec2c
Wendel Scardua <wendelscardua@gmail.com>
2023-11-15 23:21:19 -0300

    Fix Title screen metasprites offset #120

--- src/title-screen.hpp
+++ src/title-screen.hpp
@@ -12,13 +12,13 @@ public:
   static const s16 HOW_TO_SCROLL = 0x0;
   static const s16 PALETTE_SWAP_POINT = 0xb0;
   static const s16 JR_X_POSITION = 0x1c8;
-  static const u8 JR_Y_POSITION = 0x50;
+  static const u8 JR_Y_POSITION = 0x4f;
   static const s16 MAIN_MENU_CURSOR_X_POSITION = 0x148;
   static const s16 MODE_MENU_CURSOR_X_POSITION = 0x158;
   static const s16 HOW_TO_LEFT_X_POSITION = 0x38;
-  static const u8 HOW_TO_LEFT_Y_POSITION = 0x50;
+  static const u8 HOW_TO_LEFT_Y_POSITION = 0x4f;
   static const s16 HOW_TO_RIGHT_X_POSITION = 0xb8;
-  static const u8 HOW_TO_RIGHT_Y_POSITION = 0x50;
+  static const u8 HOW_TO_RIGHT_Y_POSITION = 0x4f;
   static const u8 NEXT_TRACK_DELAY = 30;
   static const u16 TRACK_ID_POSITION = NTADR_A(28, 26);
 
