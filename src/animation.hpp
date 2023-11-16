60798af0757cea8c8ceb2b2b0709107eb11f60a2
Wendel Scardua <wendelscardua@gmail.com>
2023-11-16 00:06:06 -0300

    Replace soa with common array

--- src/animation.hpp
+++ src/animation.hpp
@@ -2,23 +2,23 @@
 
 #include "banked-asset-helpers.hpp"
 #include "common.hpp"
-#include <cstdint>
-#include <soa.h>
 
 struct AnimCell {
   const unsigned char *metasprite;
   u8 duration;
 };
 
-template <uint8_t N> class Animation {
+class Animation {
 public:
-  const soa::Array<AnimCell, N> &cells;
+  const AnimCell (&cells)[];
   u8 current_frame;
   u8 current_cell;
+  u8 length;
   bool finished;
 
-  Animation(const soa::Array<AnimCell, N> &cells)
-      : cells(cells), current_frame(0), current_cell(0), finished(false) {}
+  Animation(const AnimCell (&cells)[], u8 length)
+      : cells(cells), current_frame(0), current_cell(0), length(length),
+        finished(false) {}
 
   void reset() {
     current_frame = 0;
@@ -27,12 +27,12 @@ public:
   }
 
   void update(char x, int y) {
-    banked_oam_meta_spr(x, y, cells[current_cell]->metasprite);
+    banked_oam_meta_spr(x, y, cells[current_cell].metasprite);
     current_frame++;
-    if (current_frame >= cells[current_cell]->duration) {
+    if (current_frame >= cells[current_cell].duration) {
       current_frame = 0;
       current_cell++;
-      if (current_cell == N) {
+      if (current_cell == length) {
         current_cell = 0;
         finished = true;
       }
--- src/fruits.hpp
+++ src/fruits.hpp
@@ -108,7 +108,7 @@ class Fruits {
   Board &board;
   u16 spawn_timer;
 
-  Animation<17> splash_animation{splash_cells};
+  Animation splash_animation{splash_cells, 17};
 
 public:
   static constexpr u16 SPAWN_DELAY = 5 * 60;
