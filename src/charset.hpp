3994805eaa2755a8497312dfd93a94f64a553590
Wendel Scardua <wendelscardua@gmail.com>
2025-08-10 01:27:40 -0300

    Detect score and energy codes

---
+++ src/charset.hpp
@@ -0,0 +1,71 @@
+#include "log.hpp"
+#if __cplusplus < 202002L
+#error charset support requies C++20
+#else
+#ifndef _CHARSET_H
+#define _CHARSET_H
+
+#include <cstddef>
+
+namespace charset_impl {
+
+  template <size_t N> struct TileString {
+    char Str[N]{};
+
+    constexpr TileString(char const (&Src)[N]) {
+      for (size_t I = 0, J = 0; I < N; ++I) {
+        fake_assert(Src[I] < 0x80);
+        auto new_char = TranslateUnicode(Src[I]);
+        Str[J++] = new_char;
+      }
+    }
+
+    constexpr TileString(char16_t const (&Src)[N]) {
+      for (size_t I = 0, J = 0; I < N; ++I) {
+        fake_assert(Src[I] < 0xD800 || Src[I] > 0xDFFF);
+        auto new_char = TranslateUnicode(Src[I]);
+        Str[J++] = new_char;
+      }
+    }
+
+    constexpr TileString(char32_t const (&Src)[N]) {
+      for (size_t I = 0, J = 0; I < N; ++I) {
+        auto new_char = TranslateUnicode(Src[I]);
+        Str[J++] = new_char;
+      }
+    }
+
+    constexpr char TranslateUnicode(char32_t C) {
+      switch (C) {
+      default:
+        fake_assert(false);
+        return 0xff;
+
+      // C0 control codes are uninterpreted.
+      case 0x0000 ... 0x001f:
+        return (char)C;
+
+      case U' ':
+        return 0x02;
+      case U'!':
+        return 0x2f;
+      case U'0' ... U'9':
+        return (char)(C - U'0' + 0x64);
+      case U'a' ... U'i':
+        return (char)(C - U'a' + 0x04);
+      case U'k' ... U'y':
+        return (char)(C - U'k' + 0x0d);
+      }
+    }
+  };
+
+} // namespace charset_impl
+
+// Converts strings to tiles, use "|" to toggle between normal text (usually
+// cyan) and title text (usually gray)
+template <charset_impl::TileString S> constexpr auto operator""_ts() {
+  return S.Str;
+}
+
+#endif // not _CHARSET_H
+#endif // __cplusplus >= 202002L
--- src/log.hpp
+++ src/log.hpp
@@ -20,8 +20,18 @@ void break_mesen(u8 label);
 #define BREAK_MESEN(label)                                                     \
   do {                                                                         \
   } while (0)
+#define fake_assert(condition) ((void)0)
 #else
 #define START_MESEN_WATCH(addr) start_mesen_watch(addr)
 #define STOP_MESEN_WATCH(addr) stop_mesen_watch(addr)
 #define BREAK_MESEN(label) break_mesen(label)
+// fake assert works by basically breaking compilation if condition is false
+// ... by the simple fact that the thing usiing it can't be statically compiled
+// anymore
+#define fake_assert(condition)                                                 \
+  do {                                                                         \
+    if (!(condition)) {                                                        \
+      (*(volatile unsigned char *)(0) = (42));                                 \
+    }                                                                          \
+  } while (0)
 #endif
